6cd5425e27df54d53bc42bfc0224d7d5a73f24f7
Aravind- Techie <webapp.cookscape@gmail.com>
2026-01-06 15:22:48 +0530

    06fix9

--- src/features/admin/pages/Tasks.jsx
+++ src/features/admin/pages/Tasks.jsx
@@ -27,7 +27,7 @@ const STAGES = {
     "3D Rendered Images", "Production Payment", "Approval of Finalized Designs"
   ],
   "Production": [
-    "Quality Check Process", "Factory Production"
+    "Factory Production", "Quality Check Process"
   ],
   "Installation": [
     "Installation Work", "Completion Certificate"
