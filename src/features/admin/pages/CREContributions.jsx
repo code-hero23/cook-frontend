fd6a76aee6fac825a98c85a60c979340e1e6f49c
Aravind- Techie <webapp.cookscape@gmail.com>
2026-04-01 12:23:01 +0530

    Implement native clock pickers for Walkin Hub tduiybrgvugbyeuybimrcrthyerthyetyhcetyeuiefihefuhewuribfgebrgerbgjebrgbwergboifguyryfgbydbukedgyrtuyegytgerder7dergberbguyrtbuger8ygbyfyberlkejrtbjuv eurgtuiweruqebuaweyew7 ewvywevyerhfbejrhbguiigiaigaeruinfdijngjfkdfgnnfnhndfnjndfiiisdfiidfknasdudnnbuodiuidfiugbasdgbv vtywevegulidrergertgertgjtbhityh6uergbuerbueruigbeeirughurdfghdfghfgf7ujfghdfghrreirugw98uegf8wqge78rf87we8fg78weghf8qiwuehqhweu8fhuiqwebfouwqbeuofbqwuoebfuwebtfhrfthuawdfghdrtyertyfhegfergerhs

--- src/features/admin/pages/CREContributions.jsx
+++ src/features/admin/pages/CREContributions.jsx
@@ -160,6 +160,10 @@ const CREContributions = ({ hideHeader = false }) => {
                                 <option value="2024">2024</option>
                                 <option value="2025">2025</option>
                                 <option value="2026">2026</option>
+                                <option value="2027">2027</option>
+                                <option value="2028">2028</option>
+                                <option value="2029">2029</option>
+                                <option value="2030">2030</option>
                             </select>
                         </div>
                     </div>
