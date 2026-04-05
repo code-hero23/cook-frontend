5cd0cb92491a22e15d459d8bd5e24ccf27224103
Aravind- Techie <webapp.cookscape@gmail.com>
2026-04-05 12:28:41 +0530

    Implement native clock pickers for Walkin Hub tduiybrgvugbyeuybimrcrthyerthyetyhcetyeuiefihefuhewuribfgebrgerbgjebrgbwergboifguyryfgbydbukedgyrtuyegytgerder7dergberbguyrtbuger8ygbyfyberlkejrtbjuv eurgtuiweruqebuaweyew7 ewvywevyerhfbejrhbguiigiaigaeruinfdijngjfkdfgnnfnhndfnjndfiiisdfiidfknasdudnnbuodiuidfiugbasdgbv vtywevegulidrergertgertgjtbhityh6uergbuerbueruigbeeirughurdfghdfghfgf7ujfghdfghrreirugw98uegf8wqge78rf87we8fg78weghf8qiwuehqhweu8fhuiqwebfouwqbeuofbqwuoebfuwebtfhrfthuawdfghdrtyertyfhegfergerhsfgerghergsdgdfgdrgedrgdrersdjugyujergertguerwewvgferefwedffggsdwscadsdssdcvqweryhefrtyrgetteergertyeasertudrrtuttyutyugersefswgehjherhcfsdrbr yrtssdfhfrtyhdreterreetrhyrergrtreryrewdeerergrsrtbfbfdfedrge rdvsrf5tgeggrtt4y5dsvregergervdjhbdvdregrgvegsdfgssafcascasascasdg\ dsf aseurthyertgfrfdsfbgsersdfgsdggerdsdfrfgvbdsfggdoigsdregergergergfgsdgfgjdoaefsdfgsdgigjxcdfbvbcert5yrgddscsdesertyertrgdfdfgcrdcsdegdfgsr

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,3 @@
-// Global Event & CustomEvent Polyfill for Safari and old browsers
-(function () {
-    if (typeof window === 'undefined') return;
-    try {
-        new CustomEvent('test');
-        new Event('test');
-    } catch (e) {
-        const polyfill = function (event, params) {
-            params = params || { bubbles: false, cancelable: false, detail: undefined };
-            const evt = document.createEvent('CustomEvent');
-            evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
-            return evt;
-        };
-        polyfill.prototype = window.Event.prototype;
-        window.CustomEvent = polyfill;
-        window.Event = polyfill;
-    }
-})();
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
